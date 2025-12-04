83eac35ee2566925de8cb66329aa916c4af9eae8
LucasWalica <lucasw58@educastur.es>
2025-12-04 10:20:16 +0100

    color pallete selected

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:"home", loadComponent:() => import("./components/home/home").then(m=>m.Home)},
+    {path:"login", loadComponent:() => import("./components/auth/login/login").then(m=>m.Login)},
+    {path:"register", loadComponent:() => import("./components/auth/register/register").then(m=>m.Register)},
+    { path: "**", redirectTo: "home" },
+];
---
+++ front/tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+
+    colors:{
+      "custom1-charcoal-green": "#374030",
+      "custom1-hunter-green": "#4B5C3D",
+      "custom1-muted-olive": "#92A97F",
+      "custom1-hot-berry":"#D03073",
+      "custom1-raspberry-plum":"#BD2C88",
+      "custom1-mint": "#98FF98",
+      "custom1-emerald": "#50C878",
+      "custom1-midnight": "#2C2F33",
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
