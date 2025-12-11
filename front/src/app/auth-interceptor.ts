8be9cde44cb1ec514fbff10b7dc872cbed878d73
LucasWalica <lucasw58@educastur.es>
2025-12-11 12:49:47 +0100

    minnor changes

--- front/src/app/auth-interceptor.ts
+++ front/src/app/auth-interceptor.ts
@@ -1,13 +1,8 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
-  const token = localStorage.getItem("subasta_token");
-  if (token) {
-    req = req.clone({
-      setHeaders: {
-        Authorization: `Bearer ${token}`
-      }
-    });
-  }
+  req = req.clone({
+    withCredentials: true
+  });
   return next(req);
 };
--- front/src/app/services/auth.ts
+++ front/src/app/services/auth.ts
@@ -1,32 +1,28 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, tap } from 'rxjs';
+import { Observable } from 'rxjs';
 import { apiUrl } from './env.api';
+
 @Injectable({
   providedIn: 'root',
 })
 export class Auth {
-  apiUrl = apiUrl+"/api/user/";
-  
+  apiUrl = apiUrl + "/api/user/";
+
   constructor(private http: HttpClient) {}
 
-  register(username:string, email:string, password:string){
-    const data = {username, email, password};
-    return this.http.post(this.apiUrl+"register/", data);
+  register(username: string, email: string, password: string): Observable<any> {
+    const data = { username, email, password };
+    return this.http.post(this.apiUrl + "register/", data, { withCredentials: true });
   }
 
-  login(email:string, password:string){
-    const data = {email, password};
-    return this.http.post<any>(this.apiUrl+"login/", data).pipe(
-      tap(response => {
-        localStorage.setItem("subasta_token", response.token);
-      })
-    );
+  login(email: string, password: string): Observable<any> {
+    const data = { email, password };
+    return this.http.post(this.apiUrl + "login/", data, { withCredentials: true });
+    // no necesitas tap ni guardar token localmente
   }
 
-  logout(){
-    localStorage.removeItem("subasta_token");
-    return this.http.post(this.apiUrl+"logout/", {}).subscribe();
+  logout(): Observable<any> {
+    return this.http.post(this.apiUrl + "logout/", {}, { withCredentials: true });
   }
-
 }
